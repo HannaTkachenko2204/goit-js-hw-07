d282881fe12bf1305bd49ea5ac9c742aee11fe9a
HannaTkachenko2204 <anna.kuvakina91@gmail.com>
2023-03-03 14:47:22 +0200

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const instance = basicLightbox.create(`
-    <img src="assets/images/image.png" width="800" height="600">
-`)
 
-instance.show()
+const galleryEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const arrOfItems = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class = "gallery__item">
+            <a class = "gallery__link" href = "${original}">
+            <img 
+                src = "${preview}"
+                data-source = "${original}"
+                alt = "${description}" 
+                class = "gallery__image"
+            />
+            </a>
+        </li>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("afterbegin", arrOfItems);
+console.log(galleryEl);
+
+galleryEl.addEventListener('click', event => {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    
+    const bunnerURL = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+    <img src="${bunnerURL}" width="800" height="600">`).show()
+});
